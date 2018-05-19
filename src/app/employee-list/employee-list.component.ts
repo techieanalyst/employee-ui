ac01bb18143d5fef9fc254350aef9792187c4b85
Roen Ylagan <roenylagan@live.com>
2018-05-19 13:34:15 +0800

    final

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {EmployeeService} from '../shared/employee/employee.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-employee-list',
@@ -9,12 +10,58 @@ import {EmployeeService} from '../shared/employee/employee.service';
 export class EmployeeListComponent implements OnInit {
 
   employees: Array<any>;
+  fileToUpload: File = null;
+  alert: any;
 
   constructor(private employeeService: EmployeeService) {}
 
   ngOnInit() {
-    this.employeeService.getAllEmployeesYoungerThanAttribute('25').subscribe(data => {
+
+  }
+
+  upload(files: FileList) {
+    this.fileToUpload = files.item(0);
+    this.employeeService.uploadBulkEmployees(this.fileToUpload).subscribe(data => {
       this.employees = data.employees;
+      this.alert = null;
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 0) {
+        this.alert = 'unable to connect to the service. please try again later';
+      } else {
+        this.alert = error.error.errors;
+      }
+    });
+  }
+
+  sort(attribute: string) {
+    this.employeeService.sortAllEmployeesByAttribute(attribute).subscribe(data => {
+      this.employees = data.employees;
+      this.alert = null;
+      if (this.employees.length === 0) {
+        this.alert = 'No entries found!';
+      }
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 0) {
+        this.alert = 'unable to connect to the service. please try again later';
+      } else {
+        this.alert = error.error.errors;
+      }
+    });
+  }
+
+  filter(attribute: string) {
+    this.employeeService.getAllEmployeesYoungerThanAttribute(attribute).subscribe(data => {
+      this.employees = data.employees;
+      this.alert = null;
+      if (this.employees.length === 0) {
+        this.alert = 'No entries found!';
+      }
+    }, (error: HttpErrorResponse) => {
+      if (error.status === 0) {
+        this.alert = 'unable to connect to the service. please try again later';
+      } else {
+        this.alert = error.error.errors;
+      }
     });
   }
 
--- src/app/shared/employee/employee.service.ts
+++ src/app/shared/employee/employee.service.ts
@@ -7,6 +7,7 @@ export class EmployeeService {
 
   public API = 'http://localhost:8080/employee-service/v1';
   public GET_YOUNGER_THAN = '/employees/younger/than';
+  public GET_SORTED_BY = '/employees/sorted';
   public BULK_UPLOAD = '/employees/bulkupload';
 
 
@@ -21,13 +22,18 @@ export class EmployeeService {
     });
   }
 
-  uploadBulkEmployees(): Observable<any> {
-    const formData = new FormData();
-    return this.http.post(this.API + this.BULK_UPLOAD, formData, {
+  sortAllEmployeesByAttribute(attribute: string): Observable<any> {
+    return this.http.get(this.API + this.GET_SORTED_BY + '/' + attribute, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json').append('Accept', 'application/json'),
       responseType: 'json'
     });
+  }
+
+  uploadBulkEmployees(file: File): Observable<any> {
+    const fd = new FormData();
+    fd.append('file', file);
+    return this.http.post(this.API + this.BULK_UPLOAD, fd);
   }
 
 }
